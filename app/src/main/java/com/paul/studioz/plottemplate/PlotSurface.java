162f642eb09719aab85069c60f8a2f56ab33b2b5
Paul S <Paul S>
2017-04-19 22:28:07 +0300

    First mods

---
+++ app/src/main/java/com/paul/studioz/plottemplate/PlotSurface.java
@@ -0,0 +1,14 @@
+package com.paul.studioz.plottemplate;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by paul on 19-Apr-17.
+ */
+
+public class PlotSurface extends SurfaceView {
+    public PlotSurface(Context context) {
+        super(context);
+    }
+}
